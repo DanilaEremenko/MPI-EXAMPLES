fc483785c8c6b5a02ff6d5f87b64c173bf138871
Danila <danilaeremenko@mail.ru>
2021-05-18 01:50:05 +0300

    MPI-EXAMPLES: compare_graphics.py & config updated

--- compare_graphics.py
+++ compare_graphics.py
@@ -1,4 +1,3 @@
-import plotly
 import json
 
 methods = [
@@ -6,38 +5,43 @@ methods = [
     "posix",
     "mpi"
 ]
-MAX_PROC_NUM = 200
-MIN_PROC_NUM = 5
+MAX_PROC_NUM = 202
+MIN_PROC_NUM = 2
 ###############################################################
 # --------------------------- run -----------------------------
 ###############################################################
-res_path = 'res/res_compare_new.json'
+import time
 
-import subprocess
+for mean in (5,):
+    res_path = f"res/res_fair_compare_to_{MAX_PROC_NUM}_mean_{mean}.json"
 
-res_dict = {}
+    import subprocess
 
-for method in methods:
-    binary_path = f"cmake-build-debug/main_{method}"
-    res_json = f"res/res_{method}.json"
-    res_dict[method] = {'proc_num': [], 'time': []}
-    for proc_num in list(range(MIN_PROC_NUM, MAX_PROC_NUM, 5)):
-        curr_time = []
-        for repeat_num in range(5):
-            print(f"method = {method}, proc_num = {proc_num}, i = {repeat_num}".upper())
-            if method == 'mpi':
-                subprocess.call(["mpiexec", "-np", str(proc_num + 1), binary_path], stdout=subprocess.DEVNULL)
-            else:
-                subprocess.call([binary_path, str(proc_num)], stdout=subprocess.DEVNULL)
+    res_dict = {}
 
-            with open(res_json) as fp:
-                curr_res = json.load(fp=fp)
-                curr_time.append(curr_res['input_list'][0]['time'])
-        res_dict[method]['proc_num'].append(proc_num)
-        res_dict[method]['time'].append(sum(curr_time) / len(curr_time))
+    for method in methods:
+        binary_path = f"cmake-build-debug/main_{method}"
+        res_json = f"res/res_{method}.json"
+        res_dict[method] = {'proc_num': [], 'time': []}
+        for proc_num in list(range(MIN_PROC_NUM, MAX_PROC_NUM, 5)):
+            curr_time = []
+            for repeat_num in range(mean):
+                print(f"method = {method}, proc_num = {proc_num}, i = {repeat_num}".upper())
+                start_time = time.time()
+                try:
+                    if method == 'mpi':
+                        subprocess.check_output(["mpiexec", "-np", str(proc_num + 1), binary_path])
+                    else:
+                        subprocess.check_output([binary_path, str(proc_num)])
+                    curr_time.append(time.time() - start_time)
+                except subprocess.CalledProcessError as e:
+                    raise Exception(f"Test failed for {method}")
 
-with open(res_path, 'w') as fp:
-    json.dump(res_dict, fp)
+            res_dict[method]['proc_num'].append(proc_num)
+            res_dict[method]['time'].append(sum(curr_time) / len(curr_time))
+
+    with open(res_path, 'w') as fp:
+        json.dump(res_dict, fp)
 
 ###############################################################
 # --------------------------- visualize -----------------------
@@ -45,9 +49,11 @@ with open(res_path, 'w') as fp:
 # from plotly import graph_objects as go
 # from plotly.subplots import make_subplots
 #
-# res_files = ('res/res_compare_to_100_mean_1.json',
-#              'res/res_compare_to_100_mean_5.json',
-#              'res/res_compare_to_200_mean_5.json')
+# res_files = (
+#     'res/res_fair_compare_to_100_mean_1.json',
+#     'res/res_fair_compare_to_202_mean_1.json',
+#     'res/res_fair_compare_to_202_mean_5.json'
+# )
 #
 # fig = make_subplots(
 #     rows=len(methods),
@@ -64,6 +70,7 @@ with open(res_path, 'w') as fp:
 #             go.Scatter(
 #                 x=res_dict[method]['proc_num'],
 #                 y=res_dict[method]['time'],
+#                 mode="markers+lines",
 #                 name=method,
 #                 line=dict(color=color)
 #             ),
